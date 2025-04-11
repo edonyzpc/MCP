85a00fd8a953afb9e3bb093e4d1e44545c4f4391
edonyzpc <edonyzpc@yahoo.com>
2025-04-11 14:39:36 +0800

    feat: add mcp client to connect to the weather mcp server
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

---
+++ clients/mcp-client/client.py
@@ -0,0 +1,164 @@
+import os
+import asyncio
+from typing import Optional
+from contextlib import AsyncExitStack
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_mcp_adapters.tools import (
+    load_mcp_tools,
+    convert_mcp_tool_to_langchain_tool,
+)
+
+from langgraph.prebuilt import create_react_agent
+
+# from langchain.agents import create_react_agent
+from langchain_community.chat_models import ChatTongyi
+
+from dotenv import load_dotenv
+
+load_dotenv()  # load environment variables from .env
+
+
+class MCPClient:
+    def __init__(self):
+        # Initialize session and client objects
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.qwen = ChatTongyi(temperature=0.85, model="qwen-max")
+
+    async def connect_to_server(self, server_script_path: str):
+        """Connect to an MCP server
+
+        Args:
+            server_script_path: Path to the server script (.py or .js)
+        """
+        is_python = server_script_path.endswith(".py")
+        is_js = server_script_path.endswith(".js")
+        if not (is_python or is_js):
+            raise ValueError("Server script must be a .py or .js file")
+
+        command = "python" if is_python else "node"
+        server_params = StdioServerParameters(
+            command=command, args=[server_script_path], env=None
+        )
+
+        stdio_transport = await self.exit_stack.enter_async_context(
+            stdio_client(server_params)
+        )
+        self.stdio, self.write = stdio_transport
+        self.session = await self.exit_stack.enter_async_context(
+            ClientSession(self.stdio, self.write)
+        )
+
+        await self.session.initialize()
+
+        # List available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("\nConnected to server with tools:", [tool.name for tool in tools])
+
+    async def process_query(self, query: str) -> str:
+        """Process a query using Claude and available tools"""
+        messages = [{"role": "user", "content": query}]
+
+        response = await self.session.list_tools()
+        available_tools = [
+            {
+                "type": "function",
+                "name": tool.name,
+                "description": tool.description,
+                "input_schema": tool.inputSchema,
+            }
+            for tool in response.tools
+        ]
+        available_tools = [
+            convert_mcp_tool_to_langchain_tool(self.session, tool)
+            for tool in response.tools
+        ]
+
+        # Initial Qwen API call
+        agent = create_react_agent(self.qwen, available_tools)
+        response = await agent.ainvoke({"messages": query})
+
+        # Detailed processing of tool calls if you use other LLM, e.g. Claude
+        """
+        # Process response and handle tool calls
+        final_text = []
+
+        for content in response.content:
+            if content.type == 'text':
+                final_text.append(content.text)
+            elif content.type == 'tool_use':
+                tool_name = content.name
+                tool_args = content.input
+                
+                # Execute tool call
+                result = await self.session.call_tool(tool_name, tool_args)
+                final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
+
+                # Continue conversation with tool results
+                if hasattr(content, 'text') and content.text:
+                    messages.append({
+                      "role": "assistant",
+                      "content": content.text
+                    })
+                messages.append({
+                    "role": "user", 
+                    "content": result.content
+                })
+
+                # Get next response from Claude
+                response = self.anthropic.messages.create(
+                    model="claude-3-5-sonnet-20241022",
+                    max_tokens=1000,
+                    messages=messages,
+                )
+
+                final_text.append(response.content[0].text)
+
+        return "\n".join(final_text)
+        """
+        return response["messages"][-1].content
+
+    async def chat_loop(self):
+        """Run an interactive chat loop"""
+        print("\nMCP Client Started!")
+        print("Type your queries or 'quit' to exit.")
+
+        while True:
+            try:
+                query = input("\nQuery: ").strip()
+
+                if query.lower() == "quit":
+                    break
+
+                response = await self.process_query(query)
+                print("\n" + response)
+
+            except Exception as e:
+                print(f"\nError: {str(e)}")
+
+    async def cleanup(self):
+        """Clean up resources"""
+        await self.exit_stack.aclose()
+
+
+async def main():
+    if len(sys.argv) < 2:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+
+    client = MCPClient()
+    try:
+        await client.connect_to_server(sys.argv[1])
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
+
+
+if __name__ == "__main__":
+    import sys
+
+    asyncio.run(main())
